80c9633c3d368c317cdd29b4bfdb937f8851f26b
GuiselaHerranz <gu.herranz@duocuc.cl>
2025-11-20 14:41:34 -0300

    Define DetallePedido as a Room entity data class

--- app/src/main/java/com/example/huertohogarappev2/model/DetallePedido.kt
+++ app/src/main/java/com/example/huertohogarappev2/model/DetallePedido.kt
@@ -1,4 +1,11 @@
 package com.example.huertohogarappev2.model
 
-class DetallePedido {
-}
+import androidx.room.Entity
+
+@Entity(tableName = "detallepedido",
+    primaryKeys = ["pedidoId", "productoId"])
+data class DetallePedido(
+    val pedidoId: Int,
+    val productoId: Int,
+    val cantidad: Int
+)
