1bdb1388096c612466c1071ba0ea11491e7765a2
Constanza <con.paez@duocuc.cl>
2025-11-26 17:37:17 -0300

    creacion de  la pantalla registro

--- app/src/main/java/com/example/huertohogarappev2/ui/screens/RegistroScreen.kt
+++ app/src/main/java/com/example/huertohogarappev2/ui/screens/RegistroScreen.kt
@@ -1,7 +1,6 @@
 package com.example.huertohogarappev2.ui.screen
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -19,10 +18,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.huertohogarappev2.ui.components.BotonPrincipal
 import com.example.huertohogarappev2.ui.components.CampoTexto
 import com.example.huertohogarappev2.ui.components.TituloText
