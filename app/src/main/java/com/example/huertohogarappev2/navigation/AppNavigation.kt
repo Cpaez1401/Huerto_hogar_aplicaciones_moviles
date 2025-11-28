0fa4786fbce8057b7fbf59fc6a341a5bd9af3652
Constanza <con.paez@duocuc.cl>
2025-11-28 20:06:22 -0300

    agregada la barra de navegacion

--- app/src/main/java/com/example/huertohogarappev2/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/huertohogarappev2/navigation/AppNavigation.kt
@@ -1,8 +1,14 @@
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.huertohogarappev2.ui.components.BottomBar
+import com.example.huertohogarappev2.ui.screen.CarritoScreen
 import com.example.huertohogarappev2.ui.screen.LoginScreen
 import com.example.huertohogarappev2.ui.screen.RegistroScreen
 import com.example.huertohogarappev2.ui.screens.HomeScreen
@@ -11,43 +17,56 @@ import com.example.huertohogarappev2.viewmodel.LoginViewModel
 import com.example.huertohogarappev2.viewmodel.ProductoViewModel
 
 @Composable
-fun AppNavigation(){
+fun AppNavigation() {
 
     val navController = rememberNavController()
 
-    NavHost(
-        navController = navController,
-        startDestination = "login"
-    ){
-        composable("login"){
+    Scaffold(
+        bottomBar = {
+            val route = navController.currentBackStackEntryAsState().value?.destination?.route
 
-            val viewModel : LoginViewModel = viewModel()
-
-            LoginScreen(
-                viewModel = viewModel,
-                onLogin = { navController.navigate("home") },
-                onGoToRegister = { navController.navigate("registro") }
-            )
+            // Mostrar barra solo en estas pantallas
+            if (route in listOf("home", "productos", "carrito")) {
+                BottomBar(navController)
+            }
         }
+    ) { padding ->
 
-        composable("registro"){
-            RegistroScreen (
-            onRegistrar = { navController.navigate("home") },
-            onIrLogin = { navController.navigate("login") }
-            )
+        NavHost(
+            navController = navController,
+            startDestination = "login",
+            modifier = Modifier.padding(padding)
+        ) {
+
+            composable("login") {
+
+                val viewModel: LoginViewModel = viewModel()
+
+                LoginScreen(
+                    viewModel = viewModel,
+                    onLogin = { navController.navigate("home") },
+                    onGoToRegister = { navController.navigate("registro") }
+                )
+            }
+
+            composable("registro") {
+                RegistroScreen(
+                    onRegistrar = { navController.navigate("home") },
+                    onIrLogin = { navController.navigate("login") }
+                )
+            }
+
+            composable("home") {
+                HomeScreen(navController)
+            }
+
+            composable("productos") {
+                ProductosScreen(navController = navController)
+            }
+
+            composable("carrito") {
+                CarritoScreen(navController)
+            }
         }
-
-        composable("home"){
-            HomeScreen(navController)
-        }
-
-        composable("productos") {
-            val vm: ProductoViewModel = viewModel()
-            ProductosScreen(navController, vm)
-        }
-
-
     }
-
-
 }
---
+++ app/src/main/java/com/example/huertohogarappev2/ui/components/BottomBar.kt
@@ -0,0 +1,49 @@
+package com.example.huertohogarappev2.ui.components
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Store
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+@Composable
+fun BottomBar(navController: NavController) {
+
+    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
+
+    NavigationBar(
+        containerColor = Color(0xFF4CAF50),
+        contentColor = Color.White,
+        tonalElevation = 4.dp,
+        modifier = androidx.compose.ui.Modifier.height(70.dp)
+    ) {
+
+        NavigationBarItem(
+            selected = currentRoute == "home",
+            onClick = { navController.navigate("home") },
+            icon = { Icon(Icons.Default.Home, "Home") },
+            label = { Text("Inicio") }
+        )
+
+        NavigationBarItem(
+            selected = currentRoute == "productos",
+            onClick = { navController.navigate("productos") },
+            icon = { Icon(Icons.Default.Store, "Productos") },
+            label = { Text("Productos") }
+        )
+
+        NavigationBarItem(
+            selected = currentRoute == "carrito",
+            onClick = { navController.navigate("carrito") },
+            icon = { Icon(Icons.Default.ShoppingCart, "Carrito") },
+            label = { Text("Carrito") }
+        )
+    }
+}
