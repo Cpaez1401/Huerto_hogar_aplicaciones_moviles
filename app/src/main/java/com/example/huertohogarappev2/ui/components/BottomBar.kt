eb1678c4f62d5bbf6909dbcb86d2492e454cecf0
Constanza <con.paez@duocuc.cl>
2025-12-15 11:24:53 -0300

    agregado logo y modificado levemente el BottomBar

--- app/src/main/java/com/example/huertohogarappev2/ui/components/BottomBar.kt
+++ app/src/main/java/com/example/huertohogarappev2/ui/components/BottomBar.kt
@@ -9,11 +9,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.filled.Store
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.currentBackStackEntryAsState
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -26,7 +28,7 @@ fun BottomBar(navController: NavController) {
         containerColor = Color(0xFF4CAF50),
         contentColor = Color.White,
         tonalElevation = 4.dp,
-        modifier = androidx.compose.ui.Modifier.height(70.dp)
+        modifier = Modifier.navigationBarsPadding()
     ) {
 
         NavigationBarItem(
